f7325a1056fccf58b07652f3ee47e4a672732316
Md. Liakat Biswas <87108284+liakatbiswas@users.noreply.github.com>
2023-06-28 18:37:51 +0600

    contact page with loader

--- zzzzzzzzzzzz.js
+++ zzzzzzzzzzzz.js
@@ -1,20 +1,43 @@
-let contactForm = document.getElementById('contactForm');
-contactForm.addEventListener('submit', function (event) {
+let contactForm = document.getElementById("contactForm");
+
+contactForm.addEventListener("submit", async function (event) {
     event.preventDefault();
 
-    let name = document.getElementById('name').value;
-    let email = document.getElementById('email').value;
-    let phone = document.getElementById('phone').value;
-    let message = document.getElementById('message').value;
+    let name = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let phone = document.getElementById("phone").value;
+    let message = document.getElementById("message").value;
 
     if (name.length === 0) {
-        alert('Please input Name');
+        alert("Please input Name");
     } else if (email.length === 0) {
-        alert('Please enter Email');
+        alert("Please enter Email");
     } else if (phone.length === 0) {
-        alert('Please enter phone');
+        alert("Please enter phone");
+    } else {
+        let formData = {
+            fullName: name,
+            email: email,
+            phone: phone,
+            message: message,
+        };
+        let URI = "/contactRequest";
+
+        // loader show
+        document.getElementById("loading-div").classList.remove("d-none");
+        document.getElementById("content-div").classList.add("d-none");
+
+        let result = await axios.post(URI, formData);
+
+        // loader hide
+        document.getElementById("loading-div").classList.add("d-none");
+        document.getElementById("content-div").classList.remove("d-none");
+
+        if (result.status === 200 && result.data === 1) {
+            alert("Successfull");
+            contactForm.reset();
+        } else {
+            alert("Something is wrong");
+        }
     }
-
-
-
 });
