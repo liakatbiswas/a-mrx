bd1a7c4f3d2566110954c796e5008eff57b61f8e
Md. Liakat Biswas <87108284+liakatbiswas@users.noreply.github.com>
2023-06-29 23:34:53 +0600

    language & skill list done

--- app/Http/Controllers/ResumeController.php
+++ app/Http/Controllers/ResumeController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ResumeController extends Controller
 {
     public function page() {
         return view('pages.resume');
+    }
+
+    public function languageData(Request $request)  {
+        return DB::table('language')->get();
+    }
+
+    public function skillsData(Request $request)  {
+        return DB::table('skill')->get();
     }
 }
